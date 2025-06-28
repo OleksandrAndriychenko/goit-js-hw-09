f8044327bd3bb4acea5f4e2e0f8e6e9aad7600f1
Oleksandr Andriichenko <panteras88smm@gmail.com>
2025-06-28 20:24:11 +0300

    add js file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,17 @@
-let formData = {
-    email: "",
-    message: ""
-}
+
 const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = { email: '', message: '' };
+
+// Відновлення даних із localStorage при завантаженні сторінки
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+}
+
+// Делегування події input на форму
 form.addEventListener('input', (event) => {
     const { name, value } = event.target;
     if (name in formData) {
@@ -10,31 +19,3 @@ form.addEventListener('input', (event) => {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     }
 });
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log(formData);
-})
-
-
-
-
-// const form = document.querySelector('.feedback-form');
-// const STORAGE_KEY = 'feedback-form-state';
-// let formData = { email: '', message: '' };
-
-// // Відновлення даних із localStorage при завантаженні сторінки
-// const savedData = localStorage.getItem(STORAGE_KEY);
-// if (savedData) {
-//   formData = JSON.parse(savedData);
-//   form.elements.email.value = formData.email || '';
-//   form.elements.message.value = formData.message || '';
-// }
-
-// // Делегування події input на форму
-// form.addEventListener('input', (event) => {
-//   const { name, value } = event.target;
-//   if (name in formData) {
-//     formData[name] = value;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-//   }
-// });
